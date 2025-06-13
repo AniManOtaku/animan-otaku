3f3220da674ef633165e700f21b8f3ac2cd84d3a
AniManOtaku <145964940+AniManOtaku@users.noreply.github.com>
2025-06-13 21:39:09 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,14 +3,15 @@
         const screens=document.querySelectorAll('.screen');
         const aboutRuPortrait=document.getElementById('about-ru-portrait');
         const aboutRuHorizontal=document.getElementById('about-ru-horizontal');
-        /*
+        
         screens.forEach(screen=>{
                 screen.style.height='100vh';
         });
         
         aboutRuPortrait.style.height='100vh';
         aboutRuPortrait.style.border='5px solid red';
-        aboutRuHorizontal.style.height='100vh';*/
+        aboutRuHorizontal.style.height='100vh';
+        aboutRuHorizontal.style.border='4px solid purple';
     }
     
     window.addEventListener ('resize', adjustHeights);
